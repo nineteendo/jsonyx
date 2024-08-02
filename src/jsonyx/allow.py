33e1325225eaf4d6264855f5e5c6795565136d27
Nice Zombies <nineteendo19d0@gmail.com>
2024-08-02 18:15:43 +0200

    Attempt 2

--- src/jsonyx/allow.py
+++ src/jsonyx/allow.py
@@ -13,13 +13,21 @@ __all__: list[str] = [
     "TRAILING_COMMA",
 ]
 
+#: Allow nothing
 NOTHING: frozenset[str] = frozenset()
+#: Allow comments
 COMMENTS: frozenset[str] = frozenset({"comments"})
+#: Allow duplicate keys
 DUPLICATE_KEYS: frozenset[str] = frozenset({"duplicate_keys"})
+#: Allow missing comma's
 MISSING_COMMAS: frozenset[str] = frozenset({"missing_commas"})
+#: Allow NaN and infinity
 NAN_AND_INFINITY: frozenset[str] = frozenset({"nan_and_infinity"})
+#: Allow trailing comma
 TRAILING_COMMA: frozenset[str] = frozenset({"trailing_comma"})
+#: Allow surrogates
 SURROGATES: frozenset[str] = frozenset({"surrogates"})
+#: Allow everything
 EVERYTHING: frozenset[str] = (
     COMMENTS | DUPLICATE_KEYS | MISSING_COMMAS | NAN_AND_INFINITY | SURROGATES
     | TRAILING_COMMA
