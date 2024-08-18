ed5e710f6396dfc5f8021e47aaf1eccb502cfbbc
Nice Zombies <nineteendo19d0@gmail.com>
2024-08-18 20:35:19 +0200

    Attempt 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ extensions: list[str] = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    "sphinx_toggleprompt",
 ]
 
 intersphinx_mapping: dict[str, tuple[str, None]] = {
