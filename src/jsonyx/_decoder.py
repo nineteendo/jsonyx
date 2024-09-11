0294c5224791ec52fccaa3122678c42adc530c50
Nineteendo <nineteendo19d0@gmail.com>
2024-09-11 17:08:22 +0200

    Revert "Qualify defaults"
    This reverts commit 5775616ac137b40320cafd1b2e851f60b694f616.

--- src/jsonyx/_decoder.py
+++ src/jsonyx/_decoder.py
@@ -23,7 +23,7 @@ from re import DOTALL, MULTILINE, VERBOSE, Match, RegexFlag
 from shutil import get_terminal_size
 from typing import TYPE_CHECKING, Any, Protocol, TypeVar
 
-import jsonyx.allow
+from jsonyx.allow import NOTHING
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Container
@@ -633,11 +633,7 @@ class Decoder:
     """
 
     def __init__(
-        self,
-        *,
-        allow:
-        Container[str] = jsonyx.allow.NOTHING,
-        use_decimal: bool = False,
+        self, *, allow: Container[str] = NOTHING, use_decimal: bool = False,
     ) -> None:
         """Create a new JSON decoder."""
         allow_surrogates: bool = "surrogates" in allow
