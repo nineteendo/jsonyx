128c41dcfa9603a95454bc1ef80a598282eb1011
Nineteendo <nineteendo19d0@gmail.com>
2024-09-14 15:15:06 +0200

    Use xelatex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,11 @@ html_theme_options: dict[str, Any] = {
 # -- Options for EPUB output
 epub_show_urls: str = "no"
 
+# -- Options for PDF output
+
+latex_engine: str = "xelatex"
+latex_use_xindy: bool = False
+
 # -- Options for autodoc
 
 autodoc_preserve_defaults: bool = True
