406452d2b8ce9552b7b590f342150a2732c6b31d
Wannes Boeykens <wannes.boeykens@student.kuleuven.be>
2025-03-15 18:06:32 +0100

    Test extend mode

--- src/jsonyx/test/test_paste_values.py
+++ src/jsonyx/test/test_paste_values.py
@@ -1,16 +1,34 @@
 """JSON make_patch tests."""
-# TODO(Nice Zombies): test extend, insert, set and update
+# TODO(Nice Zombies): test insert, set and update
 from __future__ import annotations
 
 __all__: list[str] = []
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
+
+import pytest
 
 from jsonyx import paste_values
 
+if TYPE_CHECKING:
+    _Operation = dict[str, Any]
 
-def test_append() -> None:
+
+@pytest.mark.parametrize(("obj", "kwargs", "expected"), [
+    ([1, 2, 3], {}, [1, 2, 3, 4]),
+    ([[1, 2, 3]], {"to": "@[0]"}, [[1, 2, 3, 4]]),
+])
+def test_append(obj: Any, kwargs: _Operation, expected: Any) -> None:
     """Test append."""
-    obj: list[Any] = [1, 2, 3]
-    paste_values(([obj], 0), 4, {"mode": "append"})
-    assert obj == [1, 2, 3, 4]
+    paste_values(([obj], 0), 4, {"mode": "append", **kwargs})
+    assert obj == expected
+
+
+@pytest.mark.parametrize(("obj", "kwargs", "expected"), [
+    ([1, 2, 3], {}, [1, 2, 3, 4, 5, 6]),
+    ([[1, 2, 3]], {"to": "@[0]"}, [[1, 2, 3, 4, 5, 6]]),
+])
+def test_extend(obj: Any, kwargs: _Operation, expected: Any) -> None:
+    """Test extend."""
+    paste_values(([obj], 0), [4, 5, 6], {"mode": "extend", **kwargs})
+    assert obj == expected
