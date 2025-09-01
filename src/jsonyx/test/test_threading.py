885d4fe2b74086a7d667f85b1b9c52560c71e5a8
Nice Zombies <nineteendo19d0@gmail.com>
2025-09-01 16:23:18 +0200

    Fix failing test

--- src/jsonyx/test/test_threading.py
+++ src/jsonyx/test/test_threading.py
@@ -3,17 +3,34 @@ from __future__ import annotations
 
 __all__: list[str] = []
 
-from collections import UserDict
 from threading import Barrier, Thread
 from typing import TYPE_CHECKING, Any
-
-import pytest
 
 import jsonyx.allow
 
 if TYPE_CHECKING:
     from collections.abc import Callable
     from types import ModuleType
+
+
+# pylint: disable-next=R0903
+class _MyMapping:
+    def __init__(self) -> None:
+        self._items: list[tuple[object, object]] = []
+        self._keys: list[object] = []
+        self._values: list[object] = []
+
+    def items(self) -> list[tuple[object, object]]:
+        """Return items."""
+        return self._items
+
+    def keys(self) -> list[object]:
+        """Return keys."""
+        return self._keys
+
+    def values(self) -> list[object]:
+        """Return values."""
+        return self._values
 
 
 def _encode_json_helper(
@@ -32,7 +49,7 @@ def _encode_json_helper(
             t.start()
 
         for _ in range(number_of_json_encodings):
-            types: dict[str, type] = {"object": UserDict}
+            types: dict[str, type] = {"object": _MyMapping}
             json.dumps(data, allow=jsonyx.allow.NON_STR_KEYS, types=types)
 
     finally:
@@ -55,15 +72,10 @@ def test_mutating_list(cjson: ModuleType) -> None:
     _encode_json_helper(cjson, worker, data)
 
 
-@pytest.mark.parametrize("object_type", [UserDict, dict])
-def test_mutating_mapping(
-    cjson: ModuleType, object_type: type[UserDict[int, int] | dict[int, int]],
-) -> None:
-    """Test mutating mapping."""
+def test_mutating_dict(cjson: ModuleType) -> None:
+    """Test mutating dict."""
     def worker(
-        barrier: Barrier,
-        data: list[UserDict[int, int] | dict[int, int]],
-        index: int,
+        barrier: Barrier, data: list[dict[int, int]], index: int,
     ) -> None:
         barrier.wait()
         while data:
@@ -72,7 +84,21 @@ def test_mutating_mapping(
                     d.clear()
                 else:
                     d[index] = index
-    data: list[UserDict[int, int] | dict[int, int]] = [
-        object_type(), object_type(),
-    ]
+    data: list[dict[int, int]] = [{}, {}]
+    _encode_json_helper(cjson, worker, data)
+
+
+def test_mutating_mapping(cjson: ModuleType) -> None:
+    """Test mutating mapping."""
+    def worker(
+        barrier: Barrier, data: list[_MyMapping], index: int,
+    ) -> None:
+        barrier.wait()
+        while data:
+            for d in data:
+                if len(d.items()) > 5:
+                    d.items().clear()
+                else:
+                    d.items().append((index, index))
+    data: list[_MyMapping] = [_MyMapping(), _MyMapping()]
     _encode_json_helper(cjson, worker, data)
